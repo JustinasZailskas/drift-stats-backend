e264f39f882404d5308df962e675cdcfcdc5ae8d
JustinasZailskas <justinaszailskas@gmail.com>
2025-03-06 20:38:22 +0200

    .

--- src/controllers/leagueController.js
+++ src/controllers/leagueController.js
@@ -3,24 +3,6 @@ const leagueService = require("../services/leagueService");
 const AppError = require("../utils/AppError");
 
 exports.getAllLeagues = async (req, res) => {
-  // try {
-  //   const leagueItems = await leagueService.getAllLeagues();
-  //   console.log("Items", leagueItems);
-  //   if (!leagueItems || leagueItems.length === 0) {
-  //     const error = new AppError("Elementai nerasti", 404);
-  //     return res.status(error.statusCode).json({
-  //       status: error.status,
-  //       message: error.message,
-  //     });
-  //   }
-  //   res.json(leagueItems);
-  // } catch (error) {
-  //   const internalError = new AppError("Vidinė serverio klaida", 500);
-  //   res.status(internalError.statusCode).json({
-  //     status: internalError.status,
-  //     message: internalError.message,
-  //   });
-  // }
   try {
     leagueService.getAllLeagues().then((leagueItems) => {
       if (!leagueItems) {
